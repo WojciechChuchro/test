8d1f8f65a89c71a17f0897d8042e0fbf35eb6f92
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-10-16 21:19:08 +0200

    Initial commit

---
+++ backend/src/test/java/com/shop/averse/ShopAverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.averse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+   <div className="bg-gray-200">
+    Hello world
+   </div>
+  )
+}
+
+export default App
