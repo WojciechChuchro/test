8eb9246cd5fe3117e90ed99cd372375554a5532c
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2023-11-18 18:54:12 +0100

    Feat: Create user model, repository, and fixture
    - Define User entity with attributes
    - Implement User repository for database operations
    - Add User fixture for testing purposes

---
+++ backend/src/main/java/com/shop/averse/database/fixtures/UserFixtures.java
@@ -0,0 +1,40 @@
+package com.shop.averse.database.fixtures;
+
+import com.shop.averse.database.models.User;
+import com.shop.averse.database.models.Role;
+import com.shop.averse.database.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class UserFixtures {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public CommandLineRunner initDatabase(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        return args -> {
+            userRepository.save(User.builder()
+                    .name("Alice")
+                    .surname("Smith")
+                    .email("alice@email.com")
+                    .username("alice")
+                    .password(passwordEncoder.encode("password"))
+                    .roleModel(Role.USER)
+                    .build());
+
+            userRepository.save(User.builder()
+                    .name("Bob")
+                    .surname("Johnson")
+                    .email("bob@email.com")
+                    .username("bob")
+                    .password(passwordEncoder.encode("password"))
+                    .roleModel(Role.ADMIN)
+                    .build());
+        };
+    }
+}
---
+++ backend/src/main/java/com/shop/averse/database/models/Role.java
@@ -0,0 +1,6 @@
+package com.shop.averse.database.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
